d9d584572aecf01b7b00bb6e6c4190bebee7fe41
Dima <dimalebedev6902@gmail.com>
2024-03-03 21:14:36 +0200

    added missing upload user avatar route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Body, Controller, Get, Post, Param, Delete, UseGuards } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Get,
+	Post,
+	Param,
+	Delete,
+	UseGuards,
+	UseInterceptors,
+	UploadedFile,
+	Req
+} from "@nestjs/common";
 import { CreateUserDto } from "../../dto/create-user-dto";
 import { UserService } from "./user.service";
 import { JwtAuthGuard } from "../../guards/jwt-auth.guard";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { multerUserAvatarImageUploadConfig } from "src/utils/multer/multerUserAvatarImageUploadConfig";
 
 @Controller("user")
 export class UserController {
@@ -29,5 +42,13 @@ export class UserController {
 	@UseGuards(JwtAuthGuard)
 	async deleteUser(@Param("userId") userId: string) {
 		return await this.userService.handleDeleteUser(userId);
+	}
+
+	@Post("avatar")
+	@UseGuards(JwtAuthGuard)
+	@UseInterceptors(FileInterceptor("avatar", multerUserAvatarImageUploadConfig))
+	async uploadUserAvatar(@UploadedFile() file: Express.Multer.File, @Req() req) {
+		const userId = req.user.userId;
+		return await this.userService.handleUserAvatarUpload(file, userId);
 	}
 }
